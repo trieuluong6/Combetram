422aabb181401ca4ce9621bafb25a3297cc4081e
trieuluong6 <trieuluong6@gmail.com>
2026-06-01 02:58:44 +0700

    Menu tổng

---
+++ menu.js
@@ -0,0 +1,32 @@
+// menu.js
+export const menu = [
+    { cat: "CƠM & MỲ XÀO", items: [
+        { id: 1, name: "Cơm sườn + trứng", price: 40000 }, { id: 2, name: "Cơm gà xé", price: 40000 },
+        { id: 3, name: "Cơm đùi gà chiên mắm", price: 40000 }, { id: 4, name: "Cơm chiên hải sản", price: 50000 },
+        { id: 5, name: "Cơm đùi gà xối mỡ", price: 50000 }, { id: 6, name: "Mỳ xào bò", price: 60000 },
+        { id: 7, name: "Mỳ xào hải sản", price: 60000 }, { id: 8, name: "Miến xào bò", price: 60000 },
+        { id: 9, name: "Miến xào hải sản", price: 60000 }
+    ]},
+    { cat: "CHÁO & HẢI SẢN", items: [
+        { id: 10, name: "Cháo hàu", price: 40000 }, { id: 11, name: "Cháo nghêu", price: 40000 },
+        { id: 12, name: "Cháo bò", price: 40000 }, { id: 13, name: "Nghêu hấp", price: 50000 },
+        { id: 14, name: "Nghêu xào", price: 50000 }, { id: 15, name: "Tôm hấp", price: 100000 },
+        { id: 16, name: "Mực hấp", price: 150000 }
+    ]},
+    { cat: "MÓN THÊM", items: [
+        { id: 17, name: "Trứng thêm", price: 10000 }, { id: 18, name: "Canh khổ qua", price: 10000 },
+        { id: 19, name: "Ram chả giò (1 cái)", price: 10000 }, { id: 20, name: "Cơm thêm", price: 5000 },
+        { id: 40, name: "Sườn thêm", price: 25000 }, { id: 41, name: "Đùi gà chiên mắm thêm", price: 30000 },
+        { id: 42, name: "Đùi gà xối mỡ thêm", price: 40000 }, { id: 43, name: "Gà xé thêm", price: 30000 }
+    ]},
+    { cat: "GIẢI KHÁT", items: [
+        { id: 21, name: "Nước ngọt các loại", price: 15000 }, { id: 22, name: "Nước bò húc", price: 20000 },
+        { id: 23, name: "Nước dừa tươi", price: 30000 }, { id: 24, name: "Nước khoáng lạt", price: 10000 },
+        { id: 29, name: "Nước cam tươi", price: 20000 }, { id: 30, name: "Nước suối", price: 10000 }
+    ]},
+    { cat: "BIA", items: [
+        { id: 32, name: "Bia Larue", price: 17000 }, { id: 33, name: "Bia Huda", price: 17000 },
+        { id: 34, name: "Bia Saigon", price: 17000 }, { id: 35, name: "Bia Tiger", price: 22000 },
+        { id: 36, name: "Bia Heineken", price: 25000 }
+    ]}
+];
